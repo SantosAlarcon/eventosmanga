9f082ceea94d32fd04b8f951d76e58427d91677e
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2024-11-13 21:29:29 +0100

    fix: build errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+    type: "content",
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+    }),
 });
 
 const event = defineCollection({
@@ -23,13 +23,13 @@ const event = defineCollection({
         endDate: z.coerce.date(),
         startTime: z.string(),
         endTime: z.string(),
-        heroImage: z.string().optional(),
+        heroImage: z.string(),
         location: z.string(),
         province: z.string(),
         url: z.string(),
         price: z.number(),
-	twitterProfile: z.string().optional()
-    })
-})
+        twitterProfile: z.string().optional(),
+    }),
+});
 
 export const collections = { blog, event };
