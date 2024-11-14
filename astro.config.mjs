c9a3c40cbd30b55bd6c877d2efd7e9cff701846e
Santos Alarcón <santosalarcon86@gmail.com>
2024-11-14 11:46:29 +0100

    changed base key in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://santosalarcon.github.io',
-    base: "eventosmanga",
+    base: "/eventosmanga",
     image: {
         remotePatterns: [{protocol: "https"}]
     },
