541888ae090a036d7e70a642da7509c8ea550c20
Santos Alarcón <santosalarcon86@gmail.com>
2024-11-19 10:53:51 +0100

    calendar: added Suspense in calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,49 +5,53 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import esLocale from "@fullcalendar/core/locales/es";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import "../styles/calendar.css";
 
 const Calendar = () => {
-    const [events, setEvents] = useState([]);
-    const [isMobile, setIsMobile] = useState(false);
-    const [mounted, setMounted] = useState(false);
+	const [events, setEvents] = useState([]);
+	const [isMobile, setIsMobile] = useState(false);
+	const [mounted, setMounted] = useState(false);
 
-    const fetchEvents = async () => {
-        const events = await fetch("/eventosmanga/api/event").then((res) => res.json());
-        setEvents(events);
-    };
+	const fetchEvents = async () => {
+		const events = await fetch("/eventosmanga/api/event").then((res) =>
+			res.json(),
+		);
+		setEvents(events);
+	};
 
-    useEffect(() => {
-        if (window.innerWidth <= 512) {
-            setIsMobile(true);
-        } else {
-            setIsMobile(false);
-        }
+	useEffect(() => {
+		if (window.innerWidth <= 512) {
+			setIsMobile(true);
+		} else {
+			setIsMobile(false);
+		}
 
-        fetchEvents();
+		fetchEvents();
 
-        setMounted(true);
-    }, []);
+		setMounted(true);
+	}, []);
 
-    if (!mounted) return null;
+	if (!mounted) return null;
 
-    return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
-            headerToolbar={{
-                left: "prev, next",
-                center: "title",
-                right: "dayGridMonth, timeGridWeek, listWeek",
-            }}
-            initialView={isMobile ? "listWeek" : "dayGridMonth"}
-            locale={esLocale}
-            events={events}
-            eventDisplay="auto"
-            displayEventTime={false}
-            displayEventEnd={false}
-        />
-    );
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<FullCalendar
+				plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+				headerToolbar={{
+					left: "prev, next",
+					center: "title",
+					right: "dayGridMonth, timeGridWeek, listWeek",
+				}}
+				initialView={isMobile ? "listWeek" : "dayGridMonth"}
+				locale={esLocale}
+				events={events}
+				eventDisplay="auto"
+				displayEventTime={false}
+				displayEventEnd={false}
+			/>
+		</Suspense>
+	);
 };
 
 export default Calendar;
