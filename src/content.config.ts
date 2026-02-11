75fcb3be9333a8eff28a87da63b08c5fd629aed7
Santos Alarcón <santosalarcon86@gmail.com>
2026-02-11 13:28:51 +0100

    chore: cambio de la api de Zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,23 +1,24 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const event = defineCollection({
-    loader: glob({ base: "./src/content/event", pattern: "**/*.mdx" }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        startDate: z.coerce.date(),
-        endDate: z.coerce.date(),
-        startTime: z.string(),
-        endTime: z.string(),
-        heroImage: z.string(),
-        location: z.string(),
-        province: z.string(),
-        url: z.string(),
-        price: z.number(),
-        twitterProfile: z.string().optional(),
-        tagColor: z.string().optional(),
-    }),
+	loader: glob({ base: "./src/content/event", pattern: "**/*.mdx" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		startDate: z.coerce.date(),
+		endDate: z.coerce.date(),
+		startTime: z.string(),
+		endTime: z.string(),
+		heroImage: z.string(),
+		location: z.string(),
+		province: z.string(),
+		url: z.string(),
+		price: z.number(),
+		twitterProfile: z.string().optional(),
+		tagColor: z.string().optional(),
+	}),
 });
 
 export const collections = { event };
