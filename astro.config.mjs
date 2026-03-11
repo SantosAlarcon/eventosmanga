7d3c2c126415de7c41e7d2bc4686813efdeea172
Santos Alarcón <santosalarcon86@gmail.com>
2026-03-11 11:07:28 +0100

    chore: quitada la fuente Red Hat Display

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,31 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        rustCompiler: true
-    },
-    site: "https://www.eventosmanga.es",
-    prefetch: {
-        prefetchAll: true,
-    },
-    //base: "/eventosmanga/",
-    image: {
-        remotePatterns: [{ protocol: "https" }],
-    },
-    integrations: [react(), mdx({ optimize: true }), sitemap({
-        changefreq: "daily",
-        priority: 0.8,
-        lastmod: new Date(),
-    })],
+	experimental: {
+		rustCompiler: true,
+		queuedRendering: {
+			enabled: true
+		}
+	},
+	site: "https://www.eventosmanga.es",
+	prefetch: {
+		prefetchAll: true,
+	},
+	//base: "/eventosmanga/",
+	image: {
+		remotePatterns: [{ protocol: "https" }],
+	},
+	integrations: [
+		react(),
+		mdx({ optimize: true }),
+		sitemap({
+			changefreq: "daily",
+			priority: 0.8,
+			lastmod: new Date(),
+		}),
+	],
 });
