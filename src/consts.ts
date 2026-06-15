67cebbe6ad13f4c4bc588fdc93788c6abcd88bf6
Santos Alarcón <santosalarcon86@gmail.com>
2026-06-15 12:24:34 +0200

    seo: mejoras de seo

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Eventos Manga - Todos los eventos de manga de España en un sólo lugar';
-export const SITE_DESCRIPTION = 'Todos los eventos de manga de España en un solo lugar';
+export const SITE_NAME = 'Eventos Manga';
+export const SITE_DESCRIPTION = 'Encuentra todos los salones manga, convenciones anime, ferias otaku y eventos de cosplay en España. Calendario actualizado con fechas, ubicaciones, precios y más.';
+export const SITE_DESCRIPTION_SHORT = 'Todos los eventos de manga de España en un solo lugar';
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
 	const events = await getCollection('event');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site,
+		site: context.site ?? 'https://www.eventosmanga.es',
 		items: events.map((event) => ({
 			...event.data,
 			link: `/evento/${event.id}/`,
